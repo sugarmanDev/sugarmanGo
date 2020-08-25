9eb21d60c3d1d6c4b645cf6e63a570c88d4f16f5
haeun1049 <haeun1049@gmail.com>
2020-08-25 16:20:36 +0900

    200825_haeun

--- assets/js/domain.js
+++ assets/js/domain.js
@@ -2,9 +2,8 @@ var getDomain = window.location.hostname;
 
 var targetDomain = '';
 
-if(getDomain =='salk.co.kr' || getDomain == 'sugarmandev.github.io') {
-  targetDomain = 'https://gimyo.co.kr/salk';
-}
-else {
-  targetDomain = 'http://localhost/salk';
-}
+// if (getDomain == 'salk.co.kr' || getDomain == 'sugarmandev.github.io') {
+//   targetDomain = 'https://gimyo.co.kr/salk';
+// } else {
+targetDomain = 'http://localhost/sugarmanGo';
+// }
