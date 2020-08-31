c653d62865a33f6c139d0b8e51f4dfaae791db2c
haeun1049 <haeun1049@gmail.com>
2020-08-31 15:51:40 +0900

    200831_haeun

--- assets/js/domain.js
+++ assets/js/domain.js
@@ -3,7 +3,7 @@ var getDomain = window.location.hostname;
 var targetDomain = '';
 
 if (getDomain == 'sugarmango.co.kr' || getDomain == 'sugarmandev.github.io') {
-  targetDomain = 'https://gimyo.co.kr/sugarmango';
+  targetDomain = 'https://gimyo.co.kr/sugarmanGo';
 } else {
   targetDomain = 'http://localhost/sugarmanGo';
 }
